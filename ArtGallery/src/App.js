bcbb32853a1e3750c6cc05e0a5b1921604635f9e
Ryan Rasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-04-25 12:58:19 -0500

    Add files via upload

---
+++ ArtGallery/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { useSelector, useDispatch, connect } from 'react-redux';
+import { clearData, fetchData, incrementId, decrementId, inputId } from './features/dataSlice';
+import { useEffect } from 'react';
+
+const mapStateToProps = (state) => ({
+  objectId: state.data.objectId
+})
+
+function App(props) {
+  const dispatch = useDispatch()
+  const data = useSelector((state) => state.data)
+
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [props.objectId, dispatch])
+
+
+  const renderImg = () => {
+    if(data.apiData) {
+      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
+    } else {
+      return <p>image here</p>
+    }
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={() => dispatch(fetchData())}>Thunk!</button>
+        <button onClick={() => dispatch(clearData())}>Clear</button>
+        <button onClick={() => dispatch(incrementId())}>Next</button>
+        <button onClick={() => dispatch(decrementId())}>Back</button>
+      </div>
+      <input value={ data.objectId } onChange={(e) => {
+        dispatch(inputId(Number(e.target.value)))
+      }} />
+      <div>
+        {data.objectId}
+        {renderImg()}
+      </div>
+    </div>
+  );
+}
+
+export default connect(mapStateToProps)(App);
